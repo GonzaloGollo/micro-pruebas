4365241dae793403e5e980239827fe3a01257b7c
GonzaloGollo <gonzalogollo@gmail.com>
2024-04-15 15:59:59 -0300

    se reenvia el token y funciona el debug pero corta y falla con status 500

--- checkout/src/main/java/com/example/checkout/controller/CheckoutRestController.java
+++ checkout/src/main/java/com/example/checkout/controller/CheckoutRestController.java
@@ -5,6 +5,7 @@ import com.example.checkout.service.ICheckoutService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/checkout")
@@ -19,7 +20,7 @@ private ICheckoutService checkoutService;
     }
 
     @GetMapping()
-    public Checkout getCheckout(@RequestParam List<String> productsIds, @RequestHeader("X-Request-from") String requestFrom){
+    public Checkout getCheckout(@RequestParam List<String> productsIds, @RequestHeader("X-Request-from") String requestFrom, @RequestHeader() Map<String, String> headers ){
         System.out.println("Enviado desde: " + requestFrom);
         if (!requestFrom.equals("gateway")){
             return null;
